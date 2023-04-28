5581b339c6bee78edcb7ac8b57c6df9d98d4de08
RyanPython <50455574+liyunze-coding@users.noreply.github.com>
2023-04-28 22:32:58 +0800

    added discord webhook

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ const auth = (function () {
 	const channel = "YOUR_CHANNEL_HERE"; // your channel
 	const username = "YOUR_BOT_ACCOUNT_HERE"; // bot account
 	const oauth = "OAUTH HERE"; // should be xxxxxxxxxxxx from the bot account, do NOT include the 'oauth:' part
+	const discordWebHook = "https://discord.com/api/webhooks/1097176918740172881/P5gRliq5GmlUo5DH_zAZ2Ym_MJ-zk3tpeOb8gUjG-9GDR07jxs9BBCZaHFxgkXkzGziK";
 
 	return {
 		channel,
